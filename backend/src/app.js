afc886aef2671f298a3c682d946d00f77d0f27c8
Nitish Kumar <nk2332001@gmail.com>
2026-04-05 04:34:52 +0530

    feat: build authentication system with register and login

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 
+//routes
+import authRoute from './routes/auth';
+
 const app = express();
+
 
 //Middlewares
 app.use(helmet()); 
 app.use(cors());
 app.use(express.json());
+
+
+//routing
+app.use('/api/auth' , authRoute);
 
 
 app.get('/api/health', (req, res) => {
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/user";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: role || "VIEWER",
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        success: true,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user),
+      });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid Email or Password" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
